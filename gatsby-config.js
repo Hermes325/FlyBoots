ad2759f2942cc26ed36cd9a6eb0cf64ea33ef2a8
Vitaly <msk.vitaly@gmail.com>
2023-01-11 23:28:37 +0300

    fix CNAME

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,15 @@ module.exports = {
     title: `FlyBoots`,
     siteUrl: `flyboots.store`
   },
-  plugins: [{
-    resolve: 'gatsby-source-datocms',
-    options: {
-      "apiToken": "f33d0f088a4c9b2c1853dd9ac6c227"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+  plugins: [
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        "apiToken": "f33d0f088a4c9b2c1853dd9ac6c227"
+      }
+    },
+    'gatsby-plugin-cname',
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp"]
 };
